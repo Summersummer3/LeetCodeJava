9521dc78f31105754c50879cc884cff4533f7bf2
summer <techsum8@live.com>
2017-04-07 11:17:38 +0900

    Java 8 new properties

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,40 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
-        Animal a = new Human();
-        a.cry();
-        Animal b = new Animal() {
-            @Override
-            public void cry() {
-                System.out.println("anyone cry");
+//	    // write your code here
+//        Animal a = new Human();
+//        a.cry();
+//        Animal b = new Animal() {
+//            @Override
+//            public void cry() {
+//                System.out.println("anyone cry");
+//            }
+//        };
+//        b.cry();
+//        LightA l = new LightA();
+//        //Light l = new LightA();  cant find l.flag??
+//        l.open();
+//        l.cutoff();
+//        System.out.println(l.flag);
+        double n, total;
+        int m;
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNextInt()){
+            n = (double)scan.nextInt();
+            m = scan.nextInt();
+            total = 0;
+            while (m > 0){
+                total += n;
+                n = Math.sqrt(n);
+                m--;
             }
-        };
-        b.cry();
-        LightA l = new LightA();
-        //Light l = new LightA();  cant find l.flag??
-        l.open();
-        l.cutoff();
-        System.out.println(l.flag);
+            System.out.println(String.format("%.2f", total));
+        }
+
     }
 }
