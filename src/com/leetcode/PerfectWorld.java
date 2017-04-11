49c8d246d61a9099cb2e59cfdaa2d401ac3cf642
summer <techsum8@live.com>
2017-04-11 11:05:42 +0900

    perfectworld Test

--- src/com/leetcode/PerfectWorld.java
+++ src/com/leetcode/PerfectWorld.java
@@ -1,16 +1,22 @@
 package com.leetcode;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+
+import java.util.*;
 
 /**
  * Created by summer on 2017/4/10.
  */
 public class PerfectWorld {
-    public static int longsetIncreaseSeq(int[] arr){
+    public static Integer[] longsetIncreaseSeq(int[] arr){
         Integer[] dp = new Integer[arr.length];
+        Integer[] flags = new Integer[arr.length];
+        List<Integer> lst = new ArrayList<>();
+        Integer[] res;
         dp[0] = 1;
+        flags[0] = -1;
+        int max = 0;
+        int maxPos = 0;
+
         for (int i = 1;i < arr.length;i++){
             int flag = -1;
             for (int j = i - 1;j >= 0;j--){
@@ -27,8 +33,25 @@ public class PerfectWorld {
             }else {
                 dp[i] = 1;
             }
+            flags[i] = flag;
         }
-        return Collections.max(Arrays.asList(dp));
+        for (int k = 0;k < dp.length; k++){
+            if (dp[k] > max){
+                max = dp[k];
+                maxPos = k;
+            }
+        }
+        int l = maxPos;
+        lst.add(arr[maxPos]);
+        while(flags[l] != -1){
+            l = flags[l];
+            lst.add(arr[l]);
+        }
+        res = new Integer[lst.size()];
+        for (int m = 0;m < lst.size();m++){
+            res[m] = lst.get(lst.size() - 1 - m);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
@@ -42,10 +65,14 @@ public class PerfectWorld {
             for (int i = 0;i < m;i++){
                 arr[i] = scan.nextInt();
             }
-            System.out.println(longsetIncreaseSeq(arr));
+            Integer[] res = longsetIncreaseSeq(arr);
+            StringBuffer sb = new StringBuffer();
+            for (Integer r:res){
+                sb.append(r.toString() + " ");
+            }
+            System.out.println(sb.toString().trim());
             n--;
         }
     }
-
 
 }
